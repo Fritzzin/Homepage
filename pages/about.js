5cd2a068992c59af4ce9407a29c35cdec4abbc33
Augusto Fritz <augusto.fritz@universo.univates.br>
2020-12-03 02:07:31 -0300

    Trying out NextJS. 2 Simple pages to try out Next's Link navigation

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Link href="/">Back to homepage</Link>
+    </div>
+  );
+}
+
+export default About;
